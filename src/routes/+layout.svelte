9427c8e09e52359187ba292dfc7f1df745eeb959
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2023-03-24 12:42:57 +0900

    feat: implement header, title

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,11 +2,29 @@
 	import { page } from '$app/stores';
 	import { generateList } from '$lib/region';
 	import './app.css';
+
+	const now = new Date(); // Rendered time in SSG
+	const heading = `${now.getFullYear()}년도 민방위 교육 일정`;
 </script>
 
+<svelte:head>
+	<title>{$page.data.regionText || '지역 선택'} - {heading}</title>
+</svelte:head>
+
 <div class="flex flex-1 flex-col bg-white sm:max-w-screen-sm">
-	<header class="p-6">
-		<h1>{new Date().getFullYear()}년도 민방위 교육 일정</h1>
+	<header class="mt-6 flex flex-col-reverse p-6">
+		<h1 class="mt-1 text-xl font-bold sm:text-2xl">{heading}</h1>
+		<div class="flex">
+			<h2 class="text-lg sm:text-xl">
+				{$page.data.regionText || '지역을 선택해주세요.'}
+			</h2>
+			<a
+				href="/"
+				class:max-sm:block={$page.url.pathname !== '/'}
+				class="my-auto ml-2 hidden rounded bg-white py-1 px-2 text-xs ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+				>변경</a
+			>
+		</div>
 	</header>
 	<div class="flex flex-1 overflow-hidden">
 		<!-- Column: Select Region -->
@@ -14,8 +32,7 @@
 			class="flex flex-col max-sm:flex-1 sm:w-64"
 			class:max-sm:hidden={$page.url.pathname !== '/'}
 		>
-			<div class="sticky top-0 bg-white p-6">지역</div>
-			<div class="flex-1 overflow-y-auto p-6 pt-0">
+			<div class="flex-1 overflow-y-auto p-6">
 				{#each generateList() as { sido: [sidoId, sidoText], sigungu }}
 					<details>
 						<summary>{sidoText}</summary>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,5 +4,6 @@ module.exports = {
 	theme: {
 		extend: {}
 	},
-	plugins: []
+	plugins: [],
+	future: { hoverOnlyWhenSupported: true }
 };
