3a7e158b7110c777f1a3f0b01942a6afd15c160d
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2023-03-24 09:54:59 +0900

    feat: implement two-column view

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,38 @@
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { generateList } from '$lib/region';
 	import './app.css';
 </script>
 
-<header class="p-6">
-	<h1>{new Date().getFullYear()}년도 민방위 교육 일정</h1>
-</header>
-
-<div class="flex flex-1 overflow-hidden">
-	<div class="flex flex-col">
-		<div class="sticky top-0 bg-white p-6">지역</div>
-		<div class="flex-1 overflow-y-auto p-6 pt-0">
-			{#each generateList() as { sido: [sidoId, sidoText], sigungu }}
-				<details>
-					<summary>{sidoText}</summary>
-					<ul>
-						{#each sigungu as [sigunguId, sigunguText]}
-							<li>
-								<a href="/{sidoId}/{sigunguId}">{sidoText} {sigunguText}</a>
-							</li>
-						{/each}
-					</ul>
-				</details>
-			{/each}
+<div class="flex flex-1 flex-col bg-white sm:max-w-screen-sm">
+	<header class="p-6">
+		<h1>{new Date().getFullYear()}년도 민방위 교육 일정</h1>
+	</header>
+	<div class="flex flex-1 overflow-hidden">
+		<!-- Column: Select Region -->
+		<div
+			class="flex flex-col max-sm:flex-1 sm:w-64"
+			class:max-sm:hidden={$page.url.pathname !== '/'}
+		>
+			<div class="sticky top-0 bg-white p-6">지역</div>
+			<div class="flex-1 overflow-y-auto p-6 pt-0">
+				{#each generateList() as { sido: [sidoId, sidoText], sigungu }}
+					<details>
+						<summary>{sidoText}</summary>
+						<ul>
+							{#each sigungu as [sigunguId, sigunguText]}
+								<li>
+									<a href="/{sidoId}/{sigunguId}">{sidoText} {sigunguText}</a>
+								</li>
+							{/each}
+						</ul>
+					</details>
+				{/each}
+			</div>
 		</div>
-	</div>
-	<div class="flex">
-		<slot />
+		<!-- Column -->
+		<div class="flex flex-1 flex-col" class:max-sm:hidden={$page.url.pathname === '/'}>
+			<slot />
+		</div>
 	</div>
 </div>
--- src/routes/[sido=id]/[sigungu=id]/+page.svelte
+++ src/routes/[sido=id]/[sigungu=id]/+page.svelte
@@ -10,27 +10,25 @@
 	);
 </script>
 
-<div class="flex flex-col">
-	<div class="sticky top-0 bg-white p-6">
-		<span>연차</span>
-		<select bind:value={selectedTarget}>
-			{#each data.targets as target}
-				<option>{target}</option>
-			{/each}
-		</select>
-	</div>
-
-	<div class="flex-1 space-y-6 overflow-y-auto p-6 pt-0">
-		{#each filteredSchedules as schedule}
-			<ul>
-				<li>날짜: {schedule.ED_YMD}</li>
-				<li>문의: {schedule.TEL_NO}</li>
-				<li>날짜: {schedule.ED_YMD}</li>
-				<li>시작: {schedule.EDU_ST_TM}</li>
-				<li>종료: {schedule.EDU_END_TM}</li>
-				<li>주소: {schedule.EDU_PLC_RDN_ADDR}</li>
-				<li>건물: {schedule.EDU_PLC_BOTTOM}</li>
-			</ul>
+<div class="sticky top-0 bg-white p-6">
+	<span>연차</span>
+	<select bind:value={selectedTarget}>
+		{#each data.targets as target}
+			<option>{target}</option>
 		{/each}
-	</div>
+	</select>
+</div>
+
+<div class="flex-1 space-y-6 overflow-y-auto p-6 pt-0">
+	{#each filteredSchedules as schedule}
+		<ul>
+			<li>날짜: {schedule.ED_YMD}</li>
+			<li>문의: {schedule.TEL_NO}</li>
+			<li>날짜: {schedule.ED_YMD}</li>
+			<li>시작: {schedule.EDU_ST_TM}</li>
+			<li>종료: {schedule.EDU_END_TM}</li>
+			<li>주소: {schedule.EDU_PLC_RDN_ADDR}</li>
+			<li>건물: {schedule.EDU_PLC_BOTTOM}</li>
+		</ul>
+	{/each}
 </div>
