7de0d002aa94e591e53481afea3b48bf7140556e
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2023-04-13 16:22:10 +0900

    chore: change template engine to liquid

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -1,21 +1,27 @@
 import { sidoData, sigunguData } from '$lib/regions';
-import * as Eta from 'eta';
+import { Liquid } from 'liquidjs';
 import type { RequestHandler } from './$types';
-import template from './template.xml?raw';
+import template from './template.liquid?raw';
+
+const domain = import.meta.env.VITE_DOMAIN;
+const engine = new Liquid();
+const lastmod = import.meta.env.VITE_NOW.substring(0, 10);
 
 export const prerender = true;
 
 export const GET = (async () => {
-	const lastmod = import.meta.env.VITE_NOW.substring(0, 10);
-	const locations: string[] = [];
+	const pathnames = ['/'];
 
 	for (const [sidoId] of sidoData) {
 		for (const sigunguId of Object.keys(sigunguData[sidoId])) {
-			locations.push(`${import.meta.env.VITE_DOMAIN}/${sidoId}/${sigunguId}`);
+			pathnames.push(`/${sidoId}/${sigunguId}`);
 		}
 	}
 
-	return new Response(await Eta.render(template, { lastmod, locations }), {
-		headers: { 'Content-Type': 'application/xml' }
-	});
+	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>\n${await engine.render(
+		engine.parse(template),
+		{ domain, lastmod, pathnames }
+	)}`;
+
+	return new Response(sitemap, { headers: { 'Content-Type': 'application/xml' } });
 }) satisfies RequestHandler;
