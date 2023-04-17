0b947004dfeba341744f2a2724fb775d35fa1eac
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2023-04-17 12:53:40 +0900

    feat: add schedule to google calendar

--- src/routes/[sido=id]/[sigungu=id]/Card.svelte
+++ src/routes/[sido=id]/[sigungu=id]/Card.svelte
@@ -7,6 +7,7 @@
 </script>
 
 <script lang="ts">
+	import { page } from '$app/stores';
 	import { onMount } from 'svelte';
 	import { slide } from 'svelte/transition';
 	import type { EducationSchedule } from '.';
@@ -46,6 +47,32 @@
 			<time datetime={startTime}>{startTime}</time>-<time datetime={endTime}>{endTime}</time>
 		</div>
 		{#if expanded}
+			{@const location = address
+				? `${address} ${schedule.EDU_PLC_BOTTOM}`
+				: schedule.EDU_PLC_BOTTOM}
+			{@const query = new URLSearchParams([
+				['action', 'TEMPLATE'],
+				['text', '민방위 교육'],
+				[
+					'dates',
+					[
+						`${schedule.ED_YMD}T${schedule.EDU_ST_TM}00`,
+						`${schedule.ED_YMD}T${schedule.EDU_END_TM}00`
+					].join('/')
+				],
+				['ctz', 'Asia/Seoul'],
+				[
+					'details',
+					[
+						`장소: ${location}\n`,
+						`대상: ${schedule.EDU_TGT_SE_NM}`,
+						`담당 공무원: ${schedule.TEL_NO}\n`,
+						`출처: ${$page.url.origin}`,
+						`기준: ${new Date().toLocaleString()}`
+					].join('\n')
+				],
+				['location', location]
+			])}
 			<div class="collapsed" transition:slide|local>
 				<div class="address">
 					{#if address}
@@ -57,10 +84,19 @@
 							{address}
 						</a>
 					{/if}
-					<span>{schedule.EDU_PLC_BOTTOM}</span>
+					{schedule.EDU_PLC_BOTTOM}
 				</div>
 				<div>
 					담당 공무원: <a on:click|stopPropagation href={telHref}>{schedule.TEL_NO}</a>
+				</div>
+
+				<div>
+					일정 등록:
+					<a
+						on:click|stopPropagation
+						href="https://www.google.com/calendar/render?{query.toString()}"
+						target="_blank">Google 캘린더</a
+					>
 				</div>
 			</div>
 		{/if}
@@ -100,8 +136,11 @@
 		text-decoration-color: #6b7280;
 		text-underline-offset: 3px;
 	}
-	.collapsed > * {
+	.collapsed {
 		padding-top: 1rem;
+		display: flex;
+		flex-direction: column;
+		row-gap: 1rem;
 	}
 	.address {
 		word-break: keep-all;
