a7209a0105af3dc5d0161adc12b3cbdf204fb059
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2023-05-13 00:07:58 +0900

    feat: build end notification

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,32 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { generateSgSendRequest } from 'sendgrid-send';
 import { defineConfig } from 'vite';
+import sendgrid from './.env.sendgrid.json' assert { type: 'json' };
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		{
+			name: 'Build End Notification',
+			buildEnd: async (error) => {
+				const message = !error
+					? `git add . && git commit -m "data: build @${Date.now()}"`
+					: error.message;
+
+				const response = await fetch(
+					generateSgSendRequest(
+						{
+							...sendgrid.body,
+							subject: `[${process.env.npm_package_name}] Build ${!error ? 'Success' : 'Fail'}`,
+							content: [{ type: 'text/html', value: `<pre>${message}</pre>` }]
+						},
+						sendgrid.key
+					)
+				);
+
+				// Reference https://docs.sendgrid.com/api-reference/mail-send/mail-send
+				if (!response.ok) console.error(await response.json());
+			}
+		}
+	]
 });
