dbd019912a44852a27af88c67d81ef26a9a658da
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2023-05-23 10:49:14 +0900

    revert: remove build notification

--- vite.config.ts
+++ vite.config.ts
@@ -1,38 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { generateSgSendRequest } from 'sendgrid-send';
 import { defineConfig } from 'vite';
-import sendgrid from './.env.sendgrid.json' assert { type: 'json' };
 
-export default defineConfig(({ command }) => ({
-	plugins: [
-		sveltekit(),
-		{
-			name: 'Build End Notification',
-			buildEnd: async (error) => {
-				if (command !== 'build') return;
+// TODO: Implement build end notification.
+// Reference https://github.com/sveltejs/kit/discussions/9939
 
-				const now = new Date();
-
-				const subject = `[${process.env.npm_package_name}] ${now.toISOString()}`;
-
-				const message = !error
-					? `git commit -m "data: build @${now.getTime()}"`
-					: error.message || 'Build failed with no error message.';
-
-				const response = await fetch(
-					generateSgSendRequest(
-						{
-							...sendgrid.body,
-							subject,
-							content: [{ type: 'text/html', value: `<pre>${message}</pre>` }]
-						},
-						sendgrid.key
-					)
-				);
-
-				// Reference https://docs.sendgrid.com/api-reference/mail-send/mail-send
-				if (!response.ok) console.error(await response.json());
-			}
-		}
-	]
-}));
+export default defineConfig(() => ({ plugins: [sveltekit()] }));
