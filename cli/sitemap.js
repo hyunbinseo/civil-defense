164f1419c568f05b49ec8b44e8166b61ff40c735
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2023-07-02 14:39:10 +0900

    feat: update sitemap after deployment

--- cli/sitemap.js
+++ cli/sitemap.js
@@ -5,6 +5,15 @@ import 'dotenv/config';
 const { PUBLIC_DOMAIN } = process.env;
 if (!PUBLIC_DOMAIN) throw new Error('PUBLIC_DOMAIN environment variable is not found.');
 
+const delayRegex = /^--delay=(\d+)$/;
+const delayMatch = process.argv.find((v) => delayRegex.test(v))?.match(delayRegex);
+
+if (delayMatch) {
+	const delay = Number(delayMatch[1]);
+	console.log(`Waiting for ${delay} second(s) for the sitemap to be updated.`);
+	await new Promise((resolve) => setTimeout(resolve, delay * 1000));
+}
+
 const response = await fetch(`https://www.google.com/ping?sitemap=${PUBLIC_DOMAIN}/sitemap.xml`);
 if (!response.ok) throw new Error(response.status.toString());
 
