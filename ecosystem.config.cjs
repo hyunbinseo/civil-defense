4bf33b270bb552612d9465cbdd8fc92c6ddf29f0
Hyunbin <47051820+hyunbinseo@users.noreply.github.com>
2023-07-05 12:17:48 +0900

    chore: add timezone check in pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,3 +1,16 @@
+/* If an error is thrown in this file, instance(s) are not launched.
+
+[PM2] Spawning PM2 daemon with ~
+[PM2] PM2 Successfully daemonized
+[PM2][ERROR] File ecosystem.config.cjs malformated
+Error: System time-zone is not UTC. (Current: Asia/Seoul)
+*/
+
+const systemTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+
+if (systemTimeZone !== 'UTC')
+	throw new Error(`System time-zone is not UTC. (Current: ${systemTimeZone})`);
+
 module.exports = {
 	apps: [
 		{
@@ -5,7 +18,7 @@ module.exports = {
 			script: './cli/build.js',
 			time: true,
 			autorestart: false,
-			cron_restart: '00 12 * * WED'
+			cron_restart: '00 03 * * WED'
 		}
 	]
 };
